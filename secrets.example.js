8048dc21cfa0c2b7dbb3dde1f72271e69f5c9453
adamjmcgrath <adamjmcgrath@gmail.com>
2017-01-22 21:19:44 +0000

    update examples

--- secrets.example.js
+++ secrets.example.js
@@ -1,25 +1,22 @@
 // Example of provider configs supported by SimpleAuth.
 
 module.exports = {
-  'google': {
-    client_id: ''
-  },
-  facebook: {
-    app_id: ''
-  },
-  twitter: {
-    consumer_key: '',
-    consumer_secret: ''
-  },
-  instagram: {
-    client_id: ''
-  },
-  tumblr: {
-    consumer_key: '',
-    consumer_secret: ''
-  },
-  'linkedin-web': {
-    client_id: '',
-    client_secret: ''
-  }
+    google: {
+        appId: '1234567890-abc.apps.googleusercontent.com',
+        callback: 'com.reactnativesimpleauth:/oauth2redirect',
+    },
+    facebook: {
+        appId: '1234567890',
+        callback: 'fb1234567890://authorize',
+    },
+    twitter: {
+        appId: '1234567890abc',
+        appSecret: '1234567890abc',
+        callback: 'reactnativesimpleauth://authorize',
+    },
+    tumblr: {
+        appId: '1234567890abc',
+        appSecret: '1234567890abc',
+        callback: 'reactnativesimpleauth://authorize',
+    },
 };
